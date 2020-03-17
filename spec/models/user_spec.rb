e8bd3d1f5d3b878ca215a127c51c7c744451d53f
kudojp <heyjudejudejude1968@gmail.com>
2020-03-17 15:31:03 +0900

    Add rspec test for User.meal_posts_feed method

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  let(:user) { create(:user) }
+
   describe 'FactoryBot' do
     it 'instantiate valid model with email, account_id, name, password' do
       user = build(:user)
@@ -34,14 +36,14 @@ RSpec.describe User, type: :model do
     end
 
     it 'is invalid to exist duplicated account_ids' do
-      user = create(:user)
+      user # instantiate from FactoryBot
       another_user = build(:user, email: 'anothertest@test.com')
       another_user.valid?
       expect(another_user.errors[:account_id]).to include('has already been taken')
     end
 
     it 'is invalid to exist duplicated names' do
-      user = create(:user)
+      user # instantiate from FactoryBot
       another_user = build(:user, account_id: 'anothertesttest')
       another_user.valid?
       expect(another_user.errors[:email]).to include('has already been taken')
@@ -112,7 +114,6 @@ RSpec.describe User, type: :model do
 
   describe '#follow(user)/#unfollow(user)' do
     it 'creates/destroys relationship in Realtionhsips table' do
-      user = create(:user)
       another = create(:user, email: 'another@test.test', account_id: 'another')
 
       # follow
@@ -127,7 +128,6 @@ RSpec.describe User, type: :model do
 
   describe '#following?(user)' do
     it 'returns array of following' do
-      user = create(:user)
       another = create(:user, email: 'another@test.test', account_id: 'another')
       user.follow(another)
 
@@ -136,6 +136,25 @@ RSpec.describe User, type: :model do
   end
 
   describe '#meal_posts_feed' do
-    # TODO
+    it 'returns meal_posts posted by followitng users in the correct order' do
+      followed_user = create(:user, email: 'followed@test.test', account_id: 'followed')
+      not_followed_user = create(:user, email: 'not_followed@test.test', account_id: 'notfollowed')
+
+      followed_old_post = create(:meal_post, user: followed_user, time: '2010/04/14 21:45:22')
+      followed_mid_post = create(:meal_post, user: followed_user, time: '2012/04/14 21:45:22')
+      followed_new_post = create(:meal_post, user: followed_user, time: '2014/04/14 21:45:22')
+
+      not_followed_old_post = create(:meal_post, user: not_followed_user, time: '2011/04/14 21:45:22')
+      not_followed_mid_post = create(:meal_post, user: not_followed_user, time: '2013/04/14 21:45:22')
+      not_followed_new_post = create(:meal_post, user: not_followed_user, time: '2015/04/14 21:45:22')
+
+      user.follow(followed_user)
+      posts_feed = user.meal_posts_feed
+
+      expect(posts_feed.size).to be(3)
+      expect(posts_feed[0]).to eq(followed_new_post)
+      expect(posts_feed[1]).to eq(followed_mid_post)
+      expect(posts_feed[2]).to eq(followed_old_post)
+    end
   end
 end
