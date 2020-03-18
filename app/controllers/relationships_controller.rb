f78adb7d8669e115390d768e36b2259d08d6c15f
kudojp <heyjudejudejude1968@gmail.com>
2020-03-19 01:05:26 +0900

    Update relationships_controller to handle the case [following already following user | unfollow not following user]

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,23 +2,43 @@ class RelationshipsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    # TODO: what if not-existing/already-following user_id is specitied?
     @followed_user = User.find(params[:followed_id])
+
+    if current_user.following?(@followed_user)
+      return respond_to do |format|
+        format.html { redirect_to @followed_user, alert: 'You are already following this user.' }
+        format.js do
+          render template: 'relationships/duplicated_create', status: :bad_request,
+                 locals: { user: @followed_user }
+        end
+      end
+    end
+
     current_user.follow(@followed_user)
 
     respond_to do |format|
-      format.html { redirect_to @followed_user }
+      format.html { redirect_to @followed_user, notice: 'You successfully followed this user.' }
       format.js
     end
   end
 
   def destroy
-    # TODO: what if not-existing/not-followign user_id is specitied?
-    @followed_user = Relationship.find(params[:id]).followed
+    @followed_user = Relationship.find_by(params[:id])&.followed
+
+    # case when current_user is not following @followed user
+    unless @followed_user
+      return respond_to do |format|
+        format.html { redirect_to @followed_user, alert: 'You are not following this user.' }
+        format.js do
+          render template: 'relationships/duplicated_destroy', status: :bad_request
+        end
+      end
+    end
+
     current_user.unfollow(@followed_user)
 
     respond_to do |format|
-      format.html { redirect_to @followed_user }
+      format.html { redirect_to @followed_user, notice: 'You successfully unfollowed this user.' }
       format.js
     end
   end
