8be04318453de563df07b00b04863a38f21d8678
kudojp <heyjudejudejude1968@gmail.com>
2020-03-18 22:57:33 +0900

    Use view helpers for users/show

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,2 +1,33 @@
 module UserHelper
+  def show_sex(user)
+    if user.is_male.nil?
+      nil
+    else
+      "Sex: #{user.is_male ? 'Male' : 'Female'}"
+    end
+  end
+
+  def show_height(user)
+    if user.height.nil?
+      nil
+    else
+      "Height: #{user.height}"
+    end
+  end
+
+  def show_weight(user)
+    if user.weight.nil?
+      nil
+    else
+      "Weight: #{user.weight}"
+    end
+  end
+
+  def show_comment(user)
+    if user.comment.nil?
+      nil
+    else
+      "Comment: #{user.comment}"
+    end
+  end
 end
