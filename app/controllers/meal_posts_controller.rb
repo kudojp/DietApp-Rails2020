c792ea7aed7fe49115b8b65653f05f6348ce643f
kudojp <heyjudejudejude1968@gmail.com>
2020-03-19 02:11:52 +0900

    Remove meal_post#index action which was acrually not user

--- app/controllers/meal_posts_controller.rb
+++ app/controllers/meal_posts_controller.rb
@@ -33,10 +33,6 @@ class MealPostsController < ApplicationController
     @meal_post = MealPost.find(params[:id])
   end
 
-  def index
-    @meal_posts = User.find(params[:id]).meal_posts
-  end
-
   private
 
   def meal_post_params
