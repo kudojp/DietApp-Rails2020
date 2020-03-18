92173bc072ab3face6b9950ab3b72644e00b4858
kudojp <heyjudejudejude1968@gmail.com>
2020-03-18 23:12:52 +0900

    Use 'find_by!' instead of 'find_by' to raise ActiveRecord::RecordNotFound

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,7 +25,7 @@ class User < ApplicationRecord
   end
 
   def unfollow(other_user)
-    active_relationships.find_by(followed_id: other_user.id).destroy
+    active_relationships.find_by!(followed_id: other_user.id).destroy
   end
 
   def following?(other_user)
