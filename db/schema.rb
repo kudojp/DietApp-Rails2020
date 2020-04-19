be3b962f76325d1deb1bf2abd716a0a3f0850c2d
kudojp <heyjudejudejude1968@gmail.com>
2020-04-19 21:34:40 +0900

    Remove null:false constaraint from Users-password

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_24_055202) do
+ActiveRecord::Schema.define(version: 2020_04_19_122131) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,7 +38,7 @@ ActiveRecord::Schema.define(version: 2020_03_24_055202) do
     t.string "email", null: false
     t.string "account_id", null: false
     t.string "name", null: false
-    t.string "encrypted_password", default: "", null: false
+    t.string "encrypted_password"
     t.boolean "is_male"
     t.float "height"
     t.float "weight"
