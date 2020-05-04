d61657369ecc0e435703d79886648a408db8ec9d
kudojp <heyjudejudejude1968@gmail.com>
2020-05-04 17:38:06 +0900

    Add rspec for FoodItem validation

--- app/models/food_item.rb
+++ app/models/food_item.rb
@@ -2,7 +2,7 @@ class FoodItem < ApplicationRecord
   belongs_to :meal_post
 
   before_validation :strip_whitespaces
-  validates :name, presence: true, length: { maximum: 50 }
+  validates :name, presence: true, length: { maximum: 30 }
   validates :amount, length: { maximum: 30 }, allow_blank: true
   validates :calory, numericality: { only_integer: true, greater_than: 0 }, allow_blank: true
 
