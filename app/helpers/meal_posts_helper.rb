1b49eef2d1924bf1833fd2d9d299f77933d486d4
kudojp <heyjudejudejude1968@gmail.com>
2020-05-04 21:28:32 +0900

    Display # and calory-sum of FoodItems belonging to each MealPost

--- app/helpers/meal_posts_helper.rb
+++ app/helpers/meal_posts_helper.rb
@@ -1,2 +1,12 @@
 module MealPostsHelper
+  def total_calories_of(meal_post)
+    return '- kcal' if meal_post.food_items_with_calories_count.zero?
+    return "#{meal_post.total_calories}kcal+" if meal_post.food_items_count != meal_post.food_items_with_calories_count
+
+    "#{meal_post.total_calories}kcal"
+  end
+
+  def total_count_and_calories_of(meal_post)
+    "#{meal_post.food_items_count}品 #{total_calories_of(meal_post)}"
+  end
 end
